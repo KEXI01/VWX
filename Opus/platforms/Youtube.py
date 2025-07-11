e3c21fcc47b05a3b7551a21846bbe9f75dd28b4b
K Σ X ! <167321927+KEXI01@users.noreply.github.com>
2025-07-11 11:42:33 +0530

    Update Youtube.py

--- Opus/platforms/Youtube.py
+++ Opus/platforms/Youtube.py
@@ -8,8 +8,8 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from TanuMusic.utils.database import is_on_off
-from TanuMusic.utils.formatters import time_to_seconds
+from Opus.utils.database import is_on_off
+from Opus.utils.formatters import time_to_seconds
 import os
 import glob
 import random
